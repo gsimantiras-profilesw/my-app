3769222a79f79b8f8c078789c11e18a7e1768599
Simantiras George <gsimantiras@profile.gr>
2019-12-19 17:03:34 +0200

    use browser=Chrome on npm test

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -22,4 +22,10 @@ describe('ListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(component.title);
+  });
 });
+ 
